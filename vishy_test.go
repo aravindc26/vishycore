a82084f6480ad0786bc0bc80a87fd9c7e4cfd12f
aravindc26 <aravindc26@gmail.com>
2015-03-16 01:38:16 +0530

    Adds en passant related test case

--- vishy_test.go
+++ vishy_test.go
@@ -3,6 +3,9 @@ package vishycore
 import "testing"
 
 func TestFENToBoardStateValidCases(t *testing.T) {
+	board2 := NewBoard()
+	board2[5][5] = pieceMap['p']
+	board2[3][5] = 0
 	cases := []struct {
 		in   string
 		want BoardState
@@ -18,11 +21,22 @@ func TestFENToBoardStateValidCases(t *testing.T) {
 				fullMoveCounter:       1,
 			},
 		},
+		{
+			"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
+			BoardState{
+				board:                 board2,
+				sideToMove:            'b',
+				castlingAbility:       "KQkq",
+				enPassantTargetSquare: "e3",
+				halfMoveClock:         0,
+				fullMoveCounter:       1,
+			},
+		},
 	}
 	for _, c := range cases {
 		got, err := NewBoardStateFromFen(c.in)
 		if err != nil {
-			t.Errorf("in: %s\n want: %+v\n error: %s\n", c.in, c.want, err)
+			t.Errorf("in: %s\n error: %s\n", c.in, err)
 		}
 		if got != c.want {
 			t.Errorf("in: %s\n want: %+v\n, got: %+v\n", c.in, c.want, got)
