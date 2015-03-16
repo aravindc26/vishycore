47d2830f2a85a7d65d4600d8781e653a8aa47166
aravindc26 <aravindc26@gmail.com>
2015-03-17 00:11:33 +0530

    Completes test case for 1.e4 c5 2.Nf3 sequence

--- vishy_test.go
+++ vishy_test.go
@@ -6,6 +6,18 @@ func TestFENToBoardStateValidCases(t *testing.T) {
 	board2 := NewBoard()
 	board2[5][5] = pieceMap['P']
 	board2[3][5] = 0
+	board3 := NewBoard()
+	board3[5][5] = pieceMap['P']
+	board3[3][5] = 0
+	board3[6][7] = pieceMap['p']
+	board3[8][7] = 0
+	board4 := NewBoard()
+	board4[5][5] = pieceMap['P']
+	board4[3][5] = 0
+	board4[6][7] = pieceMap['p']
+	board4[8][7] = 0
+	board4[4][4] = pieceMap['N']
+	board4[2][3] = 0
 	cases := []struct {
 		in   string
 		want BoardState
@@ -30,6 +42,28 @@ func TestFENToBoardStateValidCases(t *testing.T) {
 				enPassantTargetSquare: "e3",
 				halfMoveClock:         0,
 				fullMoveCounter:       1,
+			},
+		},
+		{
+			"rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2",
+			BoardState{
+				board:                 board3,
+				sideToMove:            'w',
+				castlingAbility:       "KQkq",
+				enPassantTargetSquare: "c6",
+				halfMoveClock:         0,
+				fullMoveCounter:       2,
+			},
+		},
+		{
+			"rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
+			BoardState{
+				board:                 board4,
+				sideToMove:            'b',
+				castlingAbility:       "KQkq",
+				enPassantTargetSquare: "-",
+				halfMoveClock:         1,
+				fullMoveCounter:       2,
 			},
 		},
 	}
